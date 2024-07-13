a19331c6070c9b9e25f810e7312754bc0c565cc4
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-07-13 17:22:30 +0100

    create: added checkout success logic

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,7 +3,6 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
-    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         import checkout.signals
