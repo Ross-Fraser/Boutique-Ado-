76cc3100a5a9ee94262b041d4911a2d7db67a6c1
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-07-13 16:41:17 +0100

    create: added basic checkout functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -169,5 +169,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
